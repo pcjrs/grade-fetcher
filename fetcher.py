fc3f48cd56542ec304706abab56564f06f05f992
pcjrs <46846024+pcjrs@users.noreply.github.com>
2023-11-01 21:45:47 -0400

    Update fetcher.py

--- fetcher.py
+++ fetcher.py
@@ -1,8 +1,8 @@
 # -*- coding: UTF-8 -*-
 
 """
-Author: Matteo Esposito
-Date: Fall 2019
+Author: Pierre-Claude Gacette Sanon
+Updated: Fall 2023
 """
 
 import os
@@ -16,6 +16,11 @@ from email.mime.multipart import MIMEMultipart
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+#New Additions
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.firefox import GeckoDriverManager
+#Other
 from configs.config import cfg
 from utils import timenow
 
@@ -34,19 +39,22 @@ class GradeBot:
         opts.headless = True
         self.bot = webdriver.Firefox(options=opts)
 
+
     def login(self):
         """Uses username and password to login to the student portal.
         """
         # Goto site
         bot = self.bot
-        bot.get("https://my.concordia.ca/psp/upprpr9/?cmd=login&languageCd=ENG")
+        bot.get("https://campus.concordia.ca")
         time.sleep(1)
 
         # print("Logging in...")
 
         # Locate and populate user and pwd fields.
-        user_field = bot.find_element_by_class_name('form_login_username')
-        pwd_field = bot.find_element_by_class_name('form_login_password')
+        user_field = bot.find_element(By.ID,'userNameInput')
+        pwd_field = bot.find_element(By.ID,'passwordInput')
+        
+        #Populate Login Fields
         user_field.clear()
         pwd_field.clear()
         user_field.send_keys(self.username)
@@ -54,46 +62,73 @@ class GradeBot:
         pwd_field.send_keys(Keys.RETURN)
         time.sleep(6)
 
-        if bot.current_url == 'https://my.concordia.ca/psp/upprpr9/EMPLOYEE/EMPL/h/?tab=CU_MY_FRONT_PAGE2':
+        #Test if login success or not
+        if bot.current_url == 'https://campus.concordia.ca/psc/pscsprd/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL':
             pass
         else:
+            print('Current URL:', bot.current_url)
             print('Login failed.')
             print('Re-run the program.')
             sys.exit(1)
-        # print("✓ Logged in successfully.")
+        print("✓ Login SUCCESS.")
 
         # Goto student center and find grades
-        bot.find_element_by_xpath(
-            '//a[@id="fldra_CU_MY_STUD_CENTRE" and @class="ptntop"]').click()
+        bot.find_element(By.ID, 'submenu-button-1').click()
         time.sleep(2)
-        bot.find_element_by_xpath('//a[@class="ptntop" and @role="menuitem" and @href="https://my.concordia.ca/psp/upprpr9/EMPLOYEE/EMPL/s/WEBLIB_CONCORD.CU_SIS_INFO.FieldFormula.IScript_Campus_Student_Trans?FolderPath=PORTAL_ROOT_OBJECT.CU_MY_STUD_CENTRE.CAMPUS_STUDENT_TRANSCRIPT&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder"]').click()
+        bot.find_element(By.CSS_SELECTOR, 'a[data-select-value="1030"]').click()
         time.sleep(4)
+        #print('Current URL:', bot.current_url)
+        print("✓ Grade Page ACCESSED.")
 
-    def goto_grades(self, semester, old_format=False):
+
+        #List array of semesters
+
+        #Find the HTML code block of the grades table    
+        html_code = bot.find_element(By.ID, 'SSR_DUMMY_RECV1$scroll$0').get_attribute('outerHTML')
+
+        # Parse the HTML
+        soup1 = BeautifulSoup(html_code, 'html.parser')
+
+        # Find all elements with IDs containing 'TERM_CAR$'
+        term_elements = soup1.find_all('span', {'id': lambda x: x and 'TERM_CAR$' in x})
+
+        # Extract the terms and create table
+        terms_data = {
+            'Term': [element.text for element in term_elements],
+        }
+        df_terms_data = pd.DataFrame(terms_data)
+        self.Term_List = df_terms_data['Term'].values
+
+        
+        #Print the available terms for user
+        print('✓ Terms list SUCCESS. \n', df_terms_data['Term'])
+        self.semester = input('Semester: ')
+        
+
+
+    def goto_grades(self, old_format=False):
         """Get to grade section after clicking on the radio button corresponding to the user input for 'semester'.
 
         Arguments:
             semester {String} -- Semester from Fall 2016 to Winter 2020.
             old_format {bool} -- Perform additional button click post-app update.
         """
+        #df_terms_data = pd.DataFrame(self.df_terms_data)
         bot = self.bot
-
+        
         # Change semester view.
-        bot.switch_to.frame(bot.find_element_by_name('TargetContent'))
         if not old_format:
-            bot.find_element_by_xpath('//input[@class="PSPUSHBUTTON"][@name="DERIVED_SSS_SCT_SSS_TERM_LINK"]'
-                                      '[@id="DERIVED_SSS_SCT_SSS_TERM_LINK"][@type="button"]').click()
             time.sleep(2)
 
         # Semester selection
-        if semester not in cfg['semester_mapping'].keys():
-            print('Unsupported/invalid semester. Choose semester between Fall 2016 and Winter 2020.')
+        if self.semester not in self.Term_List:
+            print('Unsupported/invalid semester. Choose semester between Fall 2015 and Winter 2023.')
             sys.exit(1)
 
-        idval = cfg['semester_mapping'][semester]
+        idval = self.Term_List.tolist().index(self.semester)
         chosen_id = "SSR_DUMMY_RECV1$sels$" + str(idval) + "$$0"
-        bot.find_element_by_xpath("//input[@id=" + "\'" + chosen_id + "\'" + "][@name='SSR_DUMMY_RECV1$sels$0'][@type='radio']").click()
-
+        bot.find_element(By.ID,chosen_id).click()
+        print('✓ Found ', self.semester, 'Term SUCCESS.')
 
     def output_vmg(self):
         """Output what is seen at 'view my grades' on myconcordia portal.
@@ -101,7 +136,8 @@ class GradeBot:
         bot = self.bot
 
         # GRADES
-        bot.find_element_by_xpath('//input[@class="PSPUSHBUTTON"][@name="DERIVED_SSS_SCT_SSR_PB_GO"][@type="button"]').click()
+        bot.find_element(By.XPATH, "//li[@data-gh-page-link='SSR_SSENRL_TERM' and @data-gh-item-link='DERIVED_SSS_SCT_SSR_PB_GO']//a").click()
+        #print('Success line 117')
 
         # Save current url to html
         time.sleep(1.5)
@@ -110,7 +146,8 @@ class GradeBot:
         site = os.getcwd() + '/grades.html'
         page = open(site)
         soup = BeautifulSoup(page.read(), features="html.parser")
-        os.remove('grades.html')
+        #os.remove('grades.html')
+        #print('Success line 127')
 
         # Parse
         grades = []
@@ -121,6 +158,7 @@ class GradeBot:
             cols = row.find_all('td')
             cols = [ele.text.strip() for ele in cols]
             grades.append([ele for ele in cols])
+            #print('Success line 139')
 
         # Convert to dataframe
         grades_df = pd.DataFrame(grades).drop([0])
@@ -129,19 +167,23 @@ class GradeBot:
         grades_df = grades_df.drop([1])
         grades_df.columns = ['Class', 'Description',
                              'Units', 'Grading', 'Letter Grade', 'Grade Points']
+        #print('Success line 146')
 
         # DISTRIBUTION
-        bot.find_element_by_xpath(
-            '//a[@class="PSHYPERLINK"][@id="ICTAB_1_54"]').click()
+        bot.find_element(By.CLASS_NAME, 'toggle.fa.fa-angle-down.label-false.ui-btn.gh-btn').click()
+        bot.find_element(By.LINK_TEXT,'Grade Distribution').click()
+        #print('Success line 151')
 
         time.sleep(1.5)
         with open('dist.html', 'w') as f:
             f.write(bot.page_source)
+            #print('Success line 156')
 
         site = os.getcwd() + '/dist.html'
         page = open(site)
         soup = BeautifulSoup(page.read(), features="html.parser")
-        os.remove('dist.html')
+        #os.remove('dist.html')
+        print('SUCCESS line 192')
 
         # Parse distribution table
         dist = []
@@ -155,17 +197,30 @@ class GradeBot:
             dist.append([ele for ele in cols])
 
         # Convert to dataframe
-        dist_df = pd.DataFrame(dist).drop([0])
-        dist_df.insert(0, 'Class', grades_df.Class)
+        dist_df = pd.DataFrame(dist)
         dist_df.columns = ['Class', 'A+', 'A', 'A-', 'B+', 'B', 'B-',
                            'C+', 'C', 'C-', 'D+', 'D', 'D-', 'F', 'FNS', 'R', 'NR']
 
-        # Fixing missing class name in cell (1, 1)
-        dist_df.Class = dist_df.Class.shift(-1)
-        dist_df.Class.loc[dist_df.shape[0]] = grades_df.Class[grades_df.shape[0]+1]
+        return grades_df, dist_df   
+        bot.quit
 
-        bot.quit()
-        return grades_df, dist_df
+
+    def goto_newsearch(self):
+        """Output a new semester on myconcordia portal.
+        """
+        bot = self.bot
+
+        # See other Terms
+        user_input = input('Would you like to see other term (Yes/No): ')
+        
+        if user_input.lower() == 'yes':
+            # Code to handle 'Yes' case
+            bot.find_element(By.XPATH, "//li[@data-gh-page-link='SSR_SSENRL_GRADE' and @data-gh-item-link='DERIVED_SSS_SCT_SSS_TERM_LINK']//a/span").click()
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print('User wants to see other term.')
+        else:
+            bot.quit()
+
 
     def send_message(self, grades_table, distribution_table, bot_pwd):
         """SMTP/ESMTP client for automated emails.
@@ -219,6 +274,7 @@ class GradeBot:
         server.login(msg['From'], bot_pwd)
         text = msg.as_string()  # You now need to convert the MIMEMultipart object to a string to send
         server.sendmail(msg['From'], msg['To'], text)
+        print('SUCCESS - Message Sent')
         server.quit()
 
 if __name__ == '__main__':
@@ -226,8 +282,7 @@ if __name__ == '__main__':
     # Accept user input for username, passwords and semester
     user = input('Username: ')
     pwd = getpass.getpass()
-    bot_pwd = getpass.getpass() #d7
-    semester = input('Semester: ')
+    #bot_pwd = getpass.getpass() #d7
     checker = GradeBot(user, pwd)
     old_grades = pd.DataFrame({"Letter Grade": ["", "", "", ""]})
 
@@ -237,9 +292,9 @@ if __name__ == '__main__':
         checker.login()
 
         # Fetch grades
-        checker.goto_grades(semester)
+        checker.goto_grades()
         grades, distribution = checker.output_vmg()
-
+        
         # Print tables depending on config option.
         if cfg['options']['console_log_tables']:
             print(grades.to_string(index=False) + '\n\n' + distribution.to_string(index=False) + '\n')
@@ -247,7 +302,7 @@ if __name__ == '__main__':
         # Compare to previous version and send email if different.
         if cfg['options']['email_notification']:
         	if list(grades['Letter Grade']) != list(old_grades['Letter Grade']):
-        		checker.send_message(grades, distribution, bot_pwd)
+        		checker.send_message(grades, distribution, pwd) #bot_pwd)
         		print(timenow() + "******Email sent.")
         	else:
         		print(timenow() + "No changes detected.")
@@ -259,3 +314,6 @@ if __name__ == '__main__':
 
         # Run every 30 min
         time.sleep(cfg['options']['time_interval'])
+
+        #See other semesters
+        #checker.goto_newsearch()
